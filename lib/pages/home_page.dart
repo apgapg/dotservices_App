b4669ce40605cba02c85f93a0401bbd9be02ef23
Ayush P Gupta <ayushpguptaapg@gmail.com>
2019-11-15 10:30:44 +0530

    Optimise performance

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -82,109 +82,107 @@ class _HomePageState extends State<HomePage> {
                       builder: (context,
                           AsyncSnapshot<List<Advertisement>> advSnapshot) {
                         if (advSnapshot.hasData && advSnapshot.data != null)
-                          return Container(
-                            child: ListView(
-                              padding: EdgeInsets.symmetric(horizontal: 8),
-                              scrollDirection: Axis.horizontal,
-                              children: [
-                                ...advSnapshot.data.map(
-                                  (Advertisement i) {
-                                    return Builder(
-                                      builder: (BuildContext context) {
-                                        return GestureDetector(
-                                          onTap: () {
-                                            if (i.client != null)
-                                              onClientTap(i.client);
-                                            else
-                                              onInfoTap();
-                                          },
-                                          child: Container(
-                                            margin: EdgeInsets.symmetric(
-                                                horizontal: 5.0),
-                                            decoration: BoxDecoration(
-                                              borderRadius:
-                                                  BorderRadius.circular(4),
+                          return ListView(
+                            padding: EdgeInsets.symmetric(horizontal: 8),
+                            scrollDirection: Axis.horizontal,
+                            children: [
+                              ...advSnapshot.data.map(
+                                (Advertisement i) {
+                                  return Builder(
+                                    builder: (BuildContext context) {
+                                      return GestureDetector(
+                                        onTap: () {
+                                          if (i.client != null)
+                                            onClientTap(i.client);
+                                          else
+                                            onInfoTap();
+                                        },
+                                        child: Container(
+                                          margin: EdgeInsets.symmetric(
+                                              horizontal: 5.0),
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(4),
+                                          ),
+                                          alignment: Alignment.center,
+                                          /*child: CachedNetworkImage(
+                                              imageUrl: i.image,
+                                              placeholder: (context, url) =>
+                                                  new CircularProgressIndicator(
+                                                strokeWidth: 1,
+                                              ),
+                                              errorWidget:
+                                                  (context, url, error) =>
+                                                      new Icon(Icons.error),
+                                            ),*/
+                                          child: ClipRRect(
+                                            borderRadius: BorderRadius.all(
+                                              Radius.circular(4),
                                             ),
-                                            alignment: Alignment.center,
-                                            /*child: CachedNetworkImage(
-                                                imageUrl: i.image,
-                                                placeholder: (context, url) =>
-                                                    new CircularProgressIndicator(
-                                                  strokeWidth: 1,
-                                                ),
-                                                errorWidget:
-                                                    (context, url, error) =>
-                                                        new Icon(Icons.error),
-                                              ),*/
-                                            child: ClipRRect(
-                                              borderRadius: BorderRadius.all(
-                                                Radius.circular(4),
-                                              ),
-                                              child: Image.network(
-                                                i.image,
-                                                loadingBuilder:
-                                                    (BuildContext context,
-                                                        Widget child,
-                                                        ImageChunkEvent
-                                                            loadingProgress) {
-                                                  if (loadingProgress == null)
-                                                    return child;
-                                                  return Container(
-                                                    width: 300,
-                                                    alignment: Alignment.center,
-                                                    child: Container(
-                                                      child: CircularProgress(),
-                                                    ),
-                                                  );
-                                                },
-                                                fit: BoxFit.fitHeight,
-                                              ),
+                                            child: Image.network(
+                                              i.image,
+                                              loadingBuilder:
+                                                  (BuildContext context,
+                                                      Widget child,
+                                                      ImageChunkEvent
+                                                          loadingProgress) {
+                                                if (loadingProgress == null)
+                                                  return child;
+                                                return Container(
+                                                  width: 300,
+                                                  alignment: Alignment.center,
+                                                  child: Container(
+                                                    child: CircularProgress(),
+                                                  ),
+                                                );
+                                              },
+                                              fit: BoxFit.fitHeight,
                                             ),
                                           ),
-                                        );
-                                      },
-                                    );
-                                  },
-                                ).toList(),
-                                InkWell(
-                                  onTap: () {
-                                    onInfoTap();
-                                  },
-                                  child: Container(
-                                    width: MediaQuery.of(context).size.width >=
-                                            600
-                                        ? MediaQuery.of(context).size.width / 3
-                                        : 300,
-                                    margin:
-                                        EdgeInsets.symmetric(horizontal: 5.0),
-                                    decoration: BoxDecoration(
-                                      color: Theme.of(context).cardColor,
-                                      borderRadius: BorderRadius.circular(4),
-                                    ),
-                                    alignment: Alignment.center,
-                                    child: Center(
-                                      child: Column(
-                                        children: <Widget>[
-                                          Icon(
-                                            Icons.live_tv,
-                                            size: 40,
-                                          ),
-                                          SizedBox(
-                                            height: 16,
-                                          ),
-                                          Text(
-                                            "Advertise here\n\nKNOW MORE >",
-                                            style: TextStyle(fontSize: 16),
-                                            textAlign: TextAlign.center,
-                                          ),
-                                        ],
-                                        mainAxisSize: MainAxisSize.min,
-                                      ),
+                                        ),
+                                      );
+                                    },
+                                  );
+                                },
+                              ).toList(),
+                              InkWell(
+                                onTap: () {
+                                  onInfoTap();
+                                },
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width >=
+                                          600
+                                      ? MediaQuery.of(context).size.width / 3
+                                      : 300,
+                                  margin:
+                                      EdgeInsets.symmetric(horizontal: 5.0),
+                                  decoration: BoxDecoration(
+                                    color: Theme.of(context).cardColor,
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: Center(
+                                    child: Column(
+                                      children: <Widget>[
+                                        Icon(
+                                          Icons.live_tv,
+                                          size: 40,
+                                        ),
+                                        SizedBox(
+                                          height: 16,
+                                        ),
+                                        Text(
+                                          "Advertise here\n\nKNOW MORE >",
+                                          style: TextStyle(fontSize: 16),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ],
+                                      mainAxisSize: MainAxisSize.min,
                                     ),
                                   ),
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           );
                         else if (advSnapshot.hasError) {
                           return StreamErrorWidget(
@@ -221,16 +219,13 @@ class _HomePageState extends State<HomePage> {
                       ],
                     )
                   else
-                    ListView(
-                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                      shrinkWrap: true,
-                      children: children
-                          .map((item) => Container(
-                                margin: EdgeInsets.symmetric(vertical: 4),
-                                child: item,
-                              ))
-                          .toList(),
-                    )
+                    ...children
+                        .map((item) => Container(
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 4, horizontal: 8),
+                              child: item,
+                            ))
+                        .toList()
                 ],
               );
             } else if (snapshot.hasError) {
